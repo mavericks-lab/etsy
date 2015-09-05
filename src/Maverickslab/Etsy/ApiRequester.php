c71ac5926b339154d399613cbdd42080c581146b
jfunu <jfunu79@gmail.com>
2015-09-05 10:40:04 +0000

    streamlining

--- src/Maverickslab/Etsy/ApiRequester.php
+++ src/Maverickslab/Etsy/ApiRequester.php
@@ -47,8 +47,7 @@ class ApiRequester {
         $requestToken = $this->oauth->getRequestToken( $this->getRequestTokenUrl (), $this->getInstallationRedirectUrl () );
 
         Session::put('request_secret', $requestToken['oauth_token_secret']);
-        return $requestToken['login_url'];
-//        return redirect($requestToken['login_url']);
+        return $requestToken;
     }
 
 
